3b41fd70ef33e25317c3d763df42408244c2174b
dongmuchen <118874800+dongmuchen@users.noreply.github.com>
2022-11-23 13:51:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUG_55-ifNatSUt6h3LDF4xr9nZ4',
+      id: '978ecfa89b81a60258b0f9c8cad40e2a',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ULH65mMkTdqHhY1vZdps8XcZ4fbbQo5ev8bQ40mTcO8	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
